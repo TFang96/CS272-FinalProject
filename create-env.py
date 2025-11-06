884b20627aa5d2842b2928482a0f081538ec8ce6
Tony Fang <78616310+TFang96@users.noreply.github.com>
2025-11-05 18:14:46 -0800

    Save environment creation method.

---
+++ create-env.py
@@ -0,0 +1,28 @@
+import gymnasium
+import highway_env
+from matplotlib import pyplot as plt
+
+def create_highway_env():
+    env = gymnasium.make('highway-v0', render_mode='rgb_array')
+    env.reset()
+    return env
+
+def create_merge_env():
+    env = gymnasium.make('merge-v0', render_mode='rgb_array')
+    env.reset()
+    return env
+
+def create_intersection_env():
+    env = gymnasium.make('intersection-v1', render_mode='rgb_array')
+    env.reset()
+    return env
+
+def main():
+    plt.imshow(create_highway_env().render())
+    plt.show()
+    plt.imshow(create_highway_env().render())
+    plt.show()
+    plt.imshow(create_intersection_env().render())
+    plt.show()
+
+main()
