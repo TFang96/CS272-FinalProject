f3158c8c609f7a63a511087f2096528981b123fb
Tony Fang <78616310+TFang96@users.noreply.github.com>
2025-11-29 10:50:11 -0800

    Revert.

--- intersection_lidar/eval_il.py
+++ intersection_lidar/eval_il.py
@@ -17,7 +17,7 @@ def make_env():
         config={
             "observation": {
                 "type": "LidarObservation",
-                "cells": 16,
+                "cells": 32,
                 "maximum_range": 64,
                 "normalise": True
             }
