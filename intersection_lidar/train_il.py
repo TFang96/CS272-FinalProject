9aa9271ea0c0a71dfe0b28e45a43dd3fef833330
Tony Fang <78616310+TFang96@users.noreply.github.com>
2025-11-29 10:49:32 -0800

    Reduce cells to speed up training.

--- intersection_lidar/train_il.py
+++ intersection_lidar/train_il.py
@@ -15,7 +15,7 @@ def make_env():
         config={
             "observation": {
                 "type": "LidarObservation",
-                "cells": 32,
+                "cells": 16,
                 "maximum_range": 64,
                 "normalise": True
             }
