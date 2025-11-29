899fb6c174a88edb383d7e408d8b3e6d25293346
Tony Fang <78616310+TFang96@users.noreply.github.com>
2025-11-29 10:31:30 -0800

    Implement training aspect **
    **- reduced lidar resolution to speed up training.

--- intersection_lidar/train_il.py
+++ intersection_lidar/train_il.py
@@ -15,7 +15,7 @@ def make_env():
         config={
             "observation": {
                 "type": "LidarObservation",
-                "cells": 128,
+                "cells": 32,
                 "maximum_range": 64,
                 "normalise": True
             }
@@ -26,14 +26,39 @@ def make_env():
 def main():
     env = make_env()
     env = Monitor(env, f"{OUTDIR}/monitor.csv")
+    policy_kwargs = dict(
+        net_arch=dict(
+            pi=[128, 128],  # policy network (actor)
+            vf=[128, 128],  # value function (critic)
+        )
+    )
 
     model = PPO(
         policy="MlpPolicy",
         env=env,
+        n_steps=1024, #shorter rollout
+        batch_size=64,
+        ent_coef=0.01, #this encourages more random exploration early on
+        learning_rate=3e-4, # if too high, we would be trusting new info too quick
+        gamma=0.99, #close to 1 -- driving needs to consider the future
+        gae_lambda=0.95, # agent doesn't get confused with moment/moment rewards
+        clip_range=0.2, #prevents policy from changing too much
+        policy_kwargs=policy_kwargs,
         verbose=1
     )
 
-    model.learn(total_timesteps=10_000)
+    # Stage 1: easy intersection
+    env.unwrapped.config.update({
+        "vehicles_count": 5,
+        # maybe no pedestrians here if they exist
+    })
+    model.learn(total_timesteps=20_000, reset_num_timesteps=True)
+
+    # Stage 2: harder intersection
+    env.unwrapped.config.update({
+        "vehicles_count": 10,
+    })
+    model.learn(total_timesteps=30_000, reset_num_timesteps=False)
     model.save(f"{OUTDIR}/model.zip")
 
     print("Training complete! Files saved to:", OUTDIR)
