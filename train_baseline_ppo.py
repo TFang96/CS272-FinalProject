206f47dd206acac8eb5ae92f6e489fdfd22e9f9d
Ryan Truong <rtruong63@gmail.com>
2025-12-01 17:54:21 -0800

    new baseline file

---
+++ train_baseline_ppo.py
@@ -0,0 +1,101 @@
+from stable_baselines3 import PPO
+import gymnasium as gym
+import highway_env
+import register_envs 
+import os
+import torch as th 
+from typing import Callable 
+
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from torch import nn 
+
+MODEL_NAME = "ppo_baseline_custom_env" 
+OUTDIR = "custom_env_training"
+FILE_NAME_ZIP = f"{MODEL_NAME}.zip"
+
+# Ensure directory exists
+os.makedirs(OUTDIR, exist_ok=True)
+
+def create_env(monitor_path=None):
+    """Creates and configures the custom roundabout environment."""
+    env = gym.make(
+        "custom-roundabout-v0",
+        render_mode="rgb_array",
+        config={
+                "observation": {
+                    "type": "Kinematics",
+                    "features_range": {
+                        "x": [-100, 100],
+                        "y": [-100, 100],
+                        "vx": [-15, 15],
+                        "vy": [-15, 15],
+                    },
+                },
+                "action": {"type": "DiscreteMetaAction", "target_speeds": [0, 5, 10, 15, 20]},
+                "incoming_vehicle_destination": None,
+                "collision_reward": -1,
+                "high_speed_reward": 0.2,
+                "right_lane_reward": 0,
+                "lane_change_reward": -0.05,
+                "screen_width": 600,
+                "screen_height": 600,
+                "centering_position": [0.5, 0.6],
+                "duration": 20,
+                "normalize_reward": True,
+            }
+    )
+    if monitor_path:
+        env = Monitor(env, monitor_path) 
+    return env
+
+
+train_env = DummyVecEnv([lambda: create_env(monitor_path=f"{OUTDIR}/monitor.csv")])
+train_env = VecNormalize(train_env, norm_obs=True, norm_reward=True, clip_obs=10.)
+
+
+# policy_kwargs = dict(
+#     activation_fn=nn.Tanh,
+#     net_arch=dict(pi=[256, 256], vf=[256, 256]) # Two layers of 256 neurons for Actor (pi) and Critic (vf)
+# )
+
+# # PPO Hyperparameters 
+# PPO_HYPERPARAMS = dict(
+#     learning_rate=linear_schedule(3e-4), 
+#     n_steps=8196,                     
+#     batch_size=256, #Higher batch size to keep stable                      
+#     gamma=0.9999,                        
+#     gae_lambda=0.95,
+#     n_epochs=10,                        
+#     ent_coef=0.02,                    
+#     vf_coef=0.5,                        
+# )
+
+
+#Base ppo with no changes
+model = PPO(
+    "MlpPolicy", 
+    train_env,
+    # policy_kwargs=policy_kwargs, 
+    verbose=1,
+    device="auto",
+    # **PPO_HYPERPARAMS
+)
+print("Starting training...")
+
+TOTAL_TIMESTEPS = 100000 
+print(f"Training for {TOTAL_TIMESTEPS} total timesteps.")
+
+model.learn(
+    total_timesteps=TOTAL_TIMESTEPS, 
+    log_interval=1,
+)
+
+
+model.save(f"{OUTDIR}/{MODEL_NAME}_final.zip")
+train_env.save(f"{OUTDIR}/vec_normalize_stats.pkl") 
+
+print(f"\nTraining finished after {model.num_timesteps} timesteps.")
+print(f"Final model saved as {MODEL_NAME}_final.zip.")
+
+train_env.close()
